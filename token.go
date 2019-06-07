a11317dc4fa4d53d6e6fcb8d8c9b9da75c2a964a
ryanjeon <yejeon2020@u.northwestern.edu>
2019-06-07 14:15:43 -0500

    Tokenizer add

---
+++ token.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Token struct {
+	Type  string
+	Value []byte
+}
+
+func isInt(word string) bool {
+	chars := strings.Split(word, "")
+
+	//Loop over characters
+	for _, c := range chars {
+		byte_char := []byte(c)
+		//one byte holds one character
+		val := byte_char[0]
+
+		//Not an integer 0:0x30 - 9:0x39
+		if val < 48 || val > 57 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func tokenizer(line string) []Token {
+	words := strings.Fields(line)
+	tokens := []Token{}
+
+	for _, w := range words {
+		if isInt(w) {
+			token := Token{
+				Type:  "Int",
+				Value: []byte(w),
+			}
+			tokens = append(tokens, token)
+		} else {
+			token := Token{
+				Type:  "Plus",
+				Value: []byte(w),
+			}
+			tokens = append(tokens, token)
+		}
+	}
+
+	return tokens
+}
+
+func main() {
+	example := "13912390123 + 1"
+	fmt.Println(tokenizer(example))
+}
